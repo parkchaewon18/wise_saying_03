18d3e22ecfdbf47ac4e0c77b91848dea13f81261
parkchaewon18 <0101angela@naver.com>
2023-03-02 10:43:37 +0900

    4단계 구현완료

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,7 +12,11 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        long lastWiseSayingId = 1;
+
         while (true){
+
+
             System.out.print("명령) ");
             //trim() : 혹시 있을지 모를 좌우공백제거된 버전으로 주세요
             String command = sc.nextLine().trim();
@@ -26,8 +30,8 @@ public class App {
                 System.out.print("작가 : ");
                 String authorName = sc.nextLine().trim();
 
-                System.out.println("1번 명령이 등록되었습니다.");
-
+                System.out.printf("%d번 명령이 등록되었습니다.\n", lastWiseSayingId);
+                lastWiseSayingId++; //증₩
 
             }
         }
